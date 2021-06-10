8b2f7f11ff221aa76d2e9addfaa13849340ee38a
ayudan11 <79744285+ayudan11@users.noreply.github.com>
2021-06-10 10:14:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ menu_list_array.sort();
 htmldata="<section class='cards'>"
 for(var i=0;i<menu_list_array.length;i++){
     htmldata=htmldata+'<div class="card">'
-    +'<img src="image/pizzaImg.png"/>'
+    +'<img src="pizzaImg.png"/>'
     +menu_list_array[i] +'</div>'
 }
 htmldata=htmldata+"</section>"
